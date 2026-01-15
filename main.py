05155e900bbaac43184ff5b4800f6465d7e85e6f
jacob sun <jacobsun211@gmail.com>
2026-01-15 14:47:25 +0200

    wrote to commands.txt and some minor changes

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import pandas as pd
 from fastapi import FastAPI, File, UploadFile
-from db import csv_validate, sort_by_danger_rate, validation
+from db import csv_validate, sort_by_danger_rate, validation_pydantic
 import uvicorn
-
 from fastapi import FastAPI
 
 app = FastAPI(title="terrorists")
 
-
-@app.post("/top-threates")
-def upload_file(file: UploadFile = File(...)):
+@app.post("/top-threats")   
+def top_threats(file: UploadFile | None = File(default=None)):
     df = csv_validate(file)
     df = pd.DataFrame(df)   
     df = sort_by_danger_rate(df)
-    return validation
+    return validation_pydantic(df)
 
     
 
